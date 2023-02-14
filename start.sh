199c32fb07cbbc1cf249da89e7acc10cf63bbe02
Sainishchai <89191791+Sainishchai@users.noreply.github.com>
2023-02-14 18:53:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # if [ -z $UPSTREAM_REPO ]
 # then
   echo "Cloning main Repository"
-  # git clone https://github.com/LazyDepveloper/Black /LazyPrincess
+  # git clone https://github.com/Sainishchai/NS_LazyPrincess /LazyPrincess
 # else
 #   echo "Cloning Custom Repo from $UPSTREAM_REPO "
 #   git clone $UPSTREAM_REPO /LazyPrincess
